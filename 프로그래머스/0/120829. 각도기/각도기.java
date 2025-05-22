c96ac636741dc4e66203594920bf8901993a9d23
김채원 <sing2727@naver.com>
2025-05-22 17:11:18 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -6,10 +6,8 @@ class Solution {
             return 2;
         } else if(angle > 90 && angle < 180) {
             return 3;
-        } else if(angle == 180) {
+        } else{
             return 4;
-        } else {
-            return 0;
         }
     }
 }
