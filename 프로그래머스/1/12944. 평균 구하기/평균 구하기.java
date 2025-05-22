ff75ddd43e573cc929051487d67d0f4e62fd0c0d
김채원 <sing2727@naver.com>
2025-05-22 11:31:08 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        double sum = 0.0;
+        
+        for(int i : arr) {
+            sum += i;
+        }
+        
+        double answer = sum/arr.length;
+        
+        return answer;
+    }
+}
