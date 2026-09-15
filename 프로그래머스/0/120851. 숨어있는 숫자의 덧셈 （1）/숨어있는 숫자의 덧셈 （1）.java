21b8c9d94f55da591352486362b70d1a6372b429
김채원 <sing2727@naver.com>
2026-09-15 17:21:23 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.32 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] str = my_string.split("");
+        
+        for(String s : str) {
+            try {
+                answer += Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                answer += 0;
+            }
+        }
+        
+        return answer;
+    }
+}
