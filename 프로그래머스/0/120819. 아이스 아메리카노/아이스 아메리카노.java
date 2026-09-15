3f608b04980a3dddfec923c601092df6b6fb2945
김채원 <sing2727@naver.com>
2026-09-15 16:37:17 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -2,15 +2,8 @@ class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
         
-        // 최대로 마실 수 있는 아메리카노의 잔 수
-        int a = money / 5500;
-        
-        // 남는 돈
-        int b = money - (5500 * a);
-        
-        // 배열에 넣기
-        answer[0] = a;
-        answer[1] = b;
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
         
         return answer;
     }
