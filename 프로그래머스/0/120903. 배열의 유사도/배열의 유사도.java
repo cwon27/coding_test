582d909455d99cd5c6a0e32e14fc728b33b85183
김채원 <sing2727@naver.com>
2026-09-17 16:23:37 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -4,14 +4,12 @@ class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
         
-        Set<String> strSet = new HashSet<>();
-        for(String s : s1) {
-            strSet.add(s);
-        }
-        
-        for(String s : s2) {
-            if(strSet.contains(s)) {
-                answer++;
+        for(String a : s1) {
+            for(String b : s2) {
+                if(a.equals(b)) {
+                    answer++;
+                    break; // 중복 안된다 했으니 있으면 걍 바로 나가기
+                }
             }
         }
         
